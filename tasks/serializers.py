8ea0f34b84bb48f2bf3c5ffb934968124bccf5f9
Rezvon1409 <rezvon0880@gmail.com>
2025-11-17 16:01:48 +0500

    push project

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,32 +2,32 @@ from rest_framework import serializers
 from .models import Category, Vacancy, Application
 from accounts.models import CustomUser
 
-# Serializer барои категорияҳо
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description']  # Намоиши ID, ном ва тавсиф
+        fields = ['id', 'name', 'description']  
 
-# Serializer барои вакансияҳо
+
 class VacancySerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)  # Намоиши категория бо маълумоти пурра
-    employer = serializers.StringRelatedField(read_only=True)  # Намоиши корфармо бо номи ӯ
-    assigned_worker = serializers.StringRelatedField(read_only=True)  # Намоиши коргари таъиншуда
+    category = CategorySerializer(read_only=True) 
+    employer = serializers.StringRelatedField(read_only=True) 
+    assigned_worker = serializers.StringRelatedField(read_only=True) 
 
     class Meta:
         model = Vacancy
         fields = ('id','title','description','salary','category','employer','assigned_worker','is_filled','created_at')
 
-# Serializer барои эҷод ва таҳрири вакансия
+
 class VacancyCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacancy
         fields = ('title','description','salary','category')
 
-# Serializer барои дархостҳо
+
 class ApplicationSerializer(serializers.ModelSerializer):
-    worker = serializers.StringRelatedField(read_only=True)  # Намоиши коргар
-    vacancy = serializers.StringRelatedField(read_only=True)  # Намоиши вакансия
+    worker = serializers.StringRelatedField(read_only=True)  
+    vacancy = serializers.StringRelatedField(read_only=True)  
 
     class Meta:
         model = Application
@@ -37,4 +37,4 @@ class ApplicationSerializer(serializers.ModelSerializer):
 class ApplicationCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
-        fields = ['vacancy']  # Коргар автомат аз user request гирифта мешавад
+        fields = ['vacancy']  
